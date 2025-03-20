a14f13d9afb7f19d4d87067b09f7d7a44e50060b
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:37:07 +0300

    Задание 1.16 выполнено

--- solutions/16-destructuring.js
+++ solutions/16-destructuring.js
@@ -6,5 +6,23 @@ const getDistance = ([x1, y1], [x2, y2]) => {
   };
 
 // BEGIN
+const getTheNearestLocation = (locations, point) => {
+    if (locations.length === 0) return null;
 
+    let nearestLocation = null;
+    let minDistance = Infinity;
+
+    for (const location of locations) {
+        const [name, coordinates] = location;
+        const distance = getDistance(coordinates, point);
+
+        if (distance < minDistance) {
+            minDistance = distance;
+            nearestLocation = location;
+        }
+    }
+
+    return nearestLocation;
+};
+export {getTheNearestLocation};
 // END
