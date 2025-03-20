46bf936ac51fc6e1793da9db03d3552677c82a00
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:35:59 +0300

    Задание 1.9 выполнено

--- solutions/9-control-statements.js
+++ solutions/9-control-statements.js
@@ -1,3 +1,14 @@
 // BEGIN
+const getTotalAmount = (money, currency) => {
+    let result = 0;
+    for (let i = 0; i < money.length; i++) {
+        const item = money[i];
+        if (item.slice(0, 3) === currency) {
+            result += Number(item.slice(4));
+        }
+    }
+    return result;
+};
 
+export default getTotalAmount;
 // END
