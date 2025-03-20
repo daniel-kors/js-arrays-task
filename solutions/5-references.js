31b7391ebe92e00ff78f50a3b77bba407308264a
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:35:00 +0300

    Задание 1.5 выполнено

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,16 @@
 // BEGIN
+const reverse = (coll) => {
+    const length = coll.length;
+    let left = 0;
+    let right = length - 1;
 
+    while (left < right) {
+        const temp = coll[left];
+        coll[left] = coll[right];
+        coll[right] = temp;
+        left++;
+        right--;
+    }
+}
+export { reverse };
 // END
