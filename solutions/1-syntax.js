c87687e9f961cda24d38a91d1d9436feae0c6518
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:33:56 +0300

    Задание 1.1 выполнено

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,14 @@
 // BEGIN (write your solution here)
+const getWeekends = (setting) => {
+    const week = ['saturday', 'sunday'];
+    const wk = ['sat', 'sun'];
+
+    if (setting === 'short') {
+        return wk;
+    }
+
+    return week;
+};
+export { getWeekends };
 
 // END
