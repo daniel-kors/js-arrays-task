3f01aba49eae8f03181dd47955513b363d6e3863
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:36:20 +0300

    Задание 1.11 выполнено

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,16 @@
 // BEGIN
+const buildDefinitionList = (definitions) => {
+    if (definitions.length === 0) {
+        return '';
+    }
 
+    let html = '<dl>';
+    for (const [term, description] of definitions) {
+        html += `<dt>${term}</dt><dd>${description}</dd>`;
+    }
+    html += '</dl>';
+    return html;
+};
+
+export default buildDefinitionList;
 // END
