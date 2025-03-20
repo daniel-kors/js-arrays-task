04aec2c2e067482bc373b51c49728608117c6f7b
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:35:28 +0300

    Задание 1.7 выполнено

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,14 @@
 // BEGIN
+const calculateAverage = (coll) => {
+    if (coll.length === 0){
+        return null;
+    }
+    let midSum = 0;
+    for (const a of coll) {
+        midSum += a;
+    }
+    return midSum / coll.length;
+};
 
+export default calculateAverage;
 // END
