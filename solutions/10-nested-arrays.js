14dbc987e078cfd682cf7b295aee4098ef995645
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:36:09 +0300

    Задание 1.10 выполнено

--- solutions/10-nested-arrays.js
+++ solutions/10-nested-arrays.js
@@ -1,3 +1,27 @@
 // BEGIN
+const getSuperSeriesWinner = (coll) => {
+    let ussrWins = 0;
+    let canadaWins = 0;
 
+
+    for (const [canadaScore, ussrScore] of coll) {
+        const result = Math.sign(canadaScore - ussrScore);
+        if (result > 0) {
+            canadaWins++;
+        } else if (result < 0) {
+            ussrWins++;
+        }
+    }
+
+    if (ussrWins > canadaWins) {
+        return 'ussr';
+    } else if (canadaWins > ussrWins) {
+        return 'canada';
+    }
+
+    return null;
+};
+
+
+export default getSuperSeriesWinner;
 // END
