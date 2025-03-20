10fd4cd7a61b769c37b77b3b46fcc306f67a9846
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:36:37 +0300

    Задание 1.13 выполнено

--- solutions/13-nested-loops.js
+++ solutions/13-nested-loops.js
@@ -1,5 +1,23 @@
 const _ = require("lodash"); 
 
 // BEGIN
+const  getSameCount = (arr1, arr2) => {
+    const coll1 = _.uniq(arr1);
+    const coll2 = _.uniq(arr2);
+    let i = 0;
 
+    for (const item1 of coll1) {
+        for (const item2 of coll2) {
+            if (item1 === item2) {
+                i += 1;
+                break;
+            }
+        }
+    }
+
+    return i;
+};
+
+
+export default getSameCount;
 // END
