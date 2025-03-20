9cc2d597fbfbc6c3ad6d4672b843417433401459
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:35:49 +0300

    Задание 1.8 выполнено

--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,21 @@
 // BEGIN
-
+const getSameParity = (coll) => {
+    const result = [];
+    if (coll.length === 0){
+        return result;
+    }
+    for (const value of coll) {
+        if (coll[0] % 2 === 0) {
+            if (value % 2 === 0) {
+                result.push(value);
+            }
+        } else {
+            if ( value % 2 !== 0 ) {
+                result.push(value);
+            }
+        }
+    }
+    return result;
+};
+export default getSameParity;
 // END
