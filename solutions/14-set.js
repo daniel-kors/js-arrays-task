f77daee436e78061a48b3a1d9a0ab8759b4c8ff1
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:36:47 +0300

    Задание 1.14 выполнено

--- solutions/14-set.js
+++ solutions/14-set.js
@@ -1,3 +1,15 @@
 // BEGIN
+const countUniqChars = (str) => {
+    const uniqueChars = [];
 
+    for (let char of str) {
+        if (!uniqueChars.includes(char)) {
+            uniqueChars.push(char);
+        }
+    }
+
+    return uniqueChars.length;
+};
+
+export default countUniqChars;
 // END
