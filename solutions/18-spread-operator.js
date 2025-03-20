b59b0ca6bd8bf035af456bd538b72795fee54b0f
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:37:39 +0300

    Задание 1.18 выполнено

--- solutions/18-spread-operator.js
+++ solutions/18-spread-operator.js
@@ -1,3 +1,16 @@
 // BEGIN
+const flatten = (coll) => {
+    let result = [];
 
+    for (const item of coll) {
+        if (Array.isArray(item)) {
+            result.push(...item);
+        } else {
+            result.push(item);
+        }
+    }
+
+    return result;
+};
+export {flatten};
 // END
