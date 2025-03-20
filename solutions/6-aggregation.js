9cbf542475fca7d361889891b1d07cfb71e5359b
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:35:16 +0300

    Задание 1.6 выполнено

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,15 @@
 // BEGIN
-
+const calculateSum = (coll) => {
+    let sum = 0;
+    if (coll.length === 0) {
+        return 0;
+    }
+    for (let i = 0; i < coll.length; i++) {
+        if (coll[i] % 3 === 0){
+            sum += coll[i];
+        }
+    }
+    return sum;
+};
+export { calculateSum };
 // END
