fa8795db357a65d0a558b45b5f1d0f237cf59052
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:36:28 +0300

    Задание 1.12 выполнено

--- solutions/12-strings.js
+++ solutions/12-strings.js
@@ -1,3 +1,20 @@
 // BEGIN
+const makeCensored = (text, errWords) => {
+    const words = text.split(' ');
+    const censoredWords = [];
+
+    for (const word of words) {
+        if (errWords.includes(word)) {
+            censoredWords.push('$#%!');
+        } else {
+            censoredWords.push(word);
+        }
+    }
+
+    return censoredWords.join(' ');
+}
+
+
+export default makeCensored;
 
 // END
