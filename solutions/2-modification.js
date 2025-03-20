b554a34bb9ad1dce7bb6126867e428b8784e9b10
Данил Абдулин <kors.daniels@yandex.ru>
2025-03-20 11:34:09 +0300

    Задание 1.2 выполнено

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,16 @@
 // BEGIN
+const swap = (coll) => {
+    if (coll.length < 2) {
+        return coll;
+    }
 
+    let first = coll[0];
+    let last = coll[coll.length - 1];
+
+    coll[0] = last;
+    coll[coll.length - 1] = first;
+
+    return coll;
+};
+export { swap };
 // END
